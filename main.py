90bbfc9671b7e88cdfc3e820689d7aaf00a828d6
Ayoub <gutinayoub@gmail.com>
2025-01-03 17:38:37 +0000

    Update main.py
    Added portfolio class

--- main.py
+++ main.py
@@ -7,14 +7,13 @@ import json
 from datetime import datetime
 
 
-
 # Investment class, as defined in the UML diagram
 class Investment:
     def __init__(self, name, purchase_price, current_price, purchase_date, investment_type):
         self.name = name
         self.purchase_price = purchase_price
         self.current_price = current_price
-        self.purchase_date = purchase_date
+        self.purchase_date = datetime.strptime(purchase_date, "%Y-%m-%d")
         self.investment_type = investment_type
 
         # Investment methods, as defined in the UML diagram
@@ -31,12 +30,69 @@ class Investment:
 
 # Child classes 
 class Stock(Investment):
-    def __init__(self, name, ticker, shares, purchase_date, purchase_price, current_price, value):
-        # call parent class
+    def __init__(self, name, ticker, shares, purchase_date, purchase_price, current_price, value, sector_type):
+# call parent class
         super().__init__(name =name, purchase_date=purchase_date, purchase_price=purchase_price, current_price=current_price)
         self.ticker = ticker
-        self.shares = shares
-        self.value = value
+        self.shares = float(shares)
+        self.value = float(value)
+        self.sector_type = sector_type
+
+# getters and setters
+        def set_shares(self):
+            self.shares = float(input("Enter how many shares you bought the investment at "))
+        
+        def set_ticker(self):
+            # will use api to get ticker from name, for now label it x
+            self.ticker = "x"
+
+        def set_sector_type(self):
+            self.sector_type = str(input("What sector is your stock in"))
+
+        # calculate abs return
+        def get_abs_return(self):
+            original_value = self.purchase_price * self.shares
+            self.value = self.shares * self.current_price
+            return self.value - original_value
+        
+
+class Crypto(Investment):
+    def __init__(self, name, symbol, quantity, contact_address, purchase_date, purchase_price, current_price):
+# call parent class
+        super().__init__(name=name, purchase_date=purchase_date, purchase_price=purchase_price, current_price=current_price)
+        self.symbol = symbol
+        self.quantity = float(quantity)
+        self.contact_address = contact_address
+
+# getters and setters
+        def set_contact_address(self):
+            # will use api to get contact address from blockchain, for now label it y 
+            self.contact_address = "y"
+
+        def set_symbol(self):
+            # will use api to get symbol from blockchain, for now label it x 
+            self.symbol = "x"
+
+        def get_market_cap():
+            # will use api for this
+            pass
+        def get_24h_volume():
+            # will use api for this
+            pass
+
+        def get_abs_return(self):
+            original_value = self.purchase_price * self.quantity
+            self.value = self.quantity * self.current_price
+            return self.value - original_value
+        
+
+class ETF(Investment):
+    def __init__(self, name, ticker, shares, expense_ratio, purchase_price, purchase_date, current_price):
+# call parent class
+        super().__init__(name=name, purchase_date=purchase_date, purchase_price=purchase_price, current_price=current_price)
+        self.ticker = ticker
+        self.shares = float(shares)
+        self.expense_ratio = float(expense_ratio)
 
         # getters and setters
         def set_shares(self):
@@ -46,8 +102,37 @@ class Stock(Investment):
             # will use api to get ticker from name, for now label it x
             self.ticker = "x"
 
-        # calculate abs return
+        def set_expense_ratio(self):
+            self.expense_ratio = float(input("Enter expense ratio"))
+
+
         def get_abs_return(self):
             original_value = self.purchase_price * self.shares
             self.value = self.shares * self.current_price
             return self.value - original_value
+        
+        
+
+class Portolio:
+    def __init__(self):
+        self.investments = []
+        self.total_value = 0
+
+    def add_investment(self, investment):
+        self.investments.append(investment)
+
+    def remove_investment(self, investment):
+        self.investments.remove(investment)
+    
+    def calculate_total_value(self):
+        return sum(investment.calculate_value() for investment in self.investments)
+    
+
+    def get_performance(self):
+        pass
+
+
+
+
+
+
